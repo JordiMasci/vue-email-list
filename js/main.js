fdfb7839e33c5ecdef4dad9a4feff3efa95b034e
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-06 15:40:41 +0200

    feat: ciclo completato

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+const { createApp } = Vue;
+
 createApp({
   data() {
     return {
       emailList: [],
     };
   },
+  mounted() {
+    for (i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => this.emailList.push(response.data.response));
+    }
+  },
 }).mount("#app");
+
+// "success":true,"response":"sarai.padberg@gmail.com"
